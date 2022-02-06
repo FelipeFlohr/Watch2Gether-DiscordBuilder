72df722a3885cd2afa4891c62727ef263f888dbd
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-02-06 20:50:09 -0300

    Added a GUI Console for the Selenium Builder using Swing

--- seleniumw2g/src/main/java/com/felipeflohr/seleniumw2g/Seleniumw2gApplication.java
+++ seleniumw2g/src/main/java/com/felipeflohr/seleniumw2g/Seleniumw2gApplication.java
@@ -3,10 +3,13 @@ package com.felipeflohr.seleniumw2g;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.felipeflohr.seleniumw2g.view.ConsoleGUI;
+
 @SpringBootApplication
 public class Seleniumw2gApplication {
 
 	public static void main(String[] args) {
+		new ConsoleGUI();
 		SpringApplication.run(Seleniumw2gApplication.class, args);
 	}
 
---
+++ seleniumw2g/src/main/java/com/felipeflohr/seleniumw2g/view/ConsoleGUI.java
@@ -0,0 +1,54 @@
+package com.felipeflohr.seleniumw2g.view;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+
+public class ConsoleGUI extends JFrame {
+	private static final long serialVersionUID = 1L;
+
+	private final JTextArea textArea;
+
+	public ConsoleGUI() {
+		if (Thread.currentThread().getId() == 1L) {
+			this.textArea = new JTextArea();
+			this.textArea.setEditable(false);
+			this.textArea.setForeground(Color.BLACK);
+			this.textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
+			this.textArea.setLineWrap(true);
+
+			getPrintStream();
+
+			// Add Components
+			this.add(textArea);
+
+			this.setLocationRelativeTo(null);
+			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			this.setSize(800, 600);
+			this.setTitle("Spring Application - Console");
+			this.setVisible(true);
+		} else {
+			this.textArea = null;
+		}
+	}
+	
+	public PrintStream getPrintStream() {
+		OutputStream outputStream = new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+				textArea.append(String.valueOf((char) b));
+			}
+		};
+		
+		PrintStream printStream = new PrintStream(outputStream);
+
+		System.setOut(printStream);
+		System.setErr(printStream);
+
+		return printStream;
+	}
+}
