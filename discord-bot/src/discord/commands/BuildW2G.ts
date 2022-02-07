9afba4767eb7a558ac7ab15a35bc7164a6c181e6
Felipe Matheus Flohr <felipeflohrlol@gmail.com>
2022-02-07 15:47:06 -0300

    Finished the Discord Bot (I guess)

---
+++ discord-bot/src/discord/commands/BuildW2G.ts
@@ -0,0 +1,134 @@
+import {Command} from "../models/Command";
+import {CommandInteraction, Message, TextBasedChannel} from "discord.js";
+import {workingSites} from "../../../links.json";
+import {Server} from "../../server/Server";
+
+export class Build implements Command{
+
+    async execute(interaction?: CommandInteraction) {
+        console.log("W2G Builder executed")
+
+        let channel: TextBasedChannel = interaction.channel
+        console.log("Going to find W2G Link")
+
+        // WIll try to find a W2G link within a range of 500 messages
+        let msgArray: Message[] = []
+        let lastMsgId: string = channel.lastMessageId;
+        let counter = 0
+        let w2gLinkFound = false
+        let w2gLink: Message
+        const counterLimit = 5
+
+        while (counter < counterLimit && !w2gLinkFound) {
+            await channel.messages.fetch({limit: 100, before: lastMsgId})
+                .then(messages => {
+                    // Will add the messages to the array
+                    messages.forEach(msg => msgArray.push(msg))
+
+                    // Will set the last message ID
+                    lastMsgId = msgArray.at(msgArray.length - 1).id
+
+                    // Will try to find any Watch2Gether link
+                    let w2gArray = msgArray.filter(msg => {
+                        let anyMatchSplit = false
+
+                        msg.content.split(" ").forEach(splitMsg => {
+                            if (splitMsg.startsWith("https://w2g.tv") || splitMsg.startsWith("https://www.watch2gether.com")) {
+                                anyMatchSplit = true
+                            }
+                        })
+
+                        return anyMatchSplit
+                    })
+
+                    console.log(`Total of Watch2Gether links: ${w2gArray.length}`)
+                    // If array > 1, then a link was found
+                    if (w2gArray.length > 0) {
+                        w2gLinkFound = true
+                        w2gLink = w2gArray.at(0)
+                        console.log("Watch2Gether link was found\n")
+                    }
+
+                    counter++
+                })
+        }
+
+        // Now it will get all videos after the last W2G Link
+        let afterW2GMessages: Message[] = []
+
+        await channel.messages.fetch({after: w2gLink.id})
+            .then(messages => {
+                messages.forEach(msg => afterW2GMessages.push(msg))
+            })
+
+        // If there is 100 messages, then it will try to find more
+        if (afterW2GMessages.length == 100) {
+            let messagesAfterHundred: Message[] = []
+            let lastMessage = afterW2GMessages.at(afterW2GMessages.length - 1)
+            let limitReached = false
+
+            while (!limitReached) {
+                await channel.messages.fetch({after: lastMessage.id})
+                    .then(messages => {
+                        let iterationArray: Message[] = []
+
+                        messages.forEach(msg => {
+                            iterationArray.push(msg)
+                        })
+
+                        if (iterationArray.length < 100) {
+                            limitReached = true
+                        }
+
+                        lastMessage = iterationArray.at(iterationArray.length - 1)
+                        iterationArray.forEach(msg => messagesAfterHundred.push(msg))
+                    })
+            }
+
+            messagesAfterHundred.forEach(msg => afterW2GMessages.push(msg))
+        }
+
+        // Now it will try to parse the messages
+        let w2gVideos: string[] = []
+        let nonW2GVideos: string[] = []
+
+        afterW2GMessages.forEach(msg => {
+            let msgSplit = msg.content.split(" ") // <- Will split each space for every message
+            msgSplit.forEach(m => {
+                const urlIsWorkingWebsite = () => { // Checks if the URL is part of the Working Websites, defined in "links.json"
+                    let anyMatch = false
+
+                    workingSites.forEach(site => {
+                        if (m.startsWith(site)) anyMatch = true;
+                    })
+
+                    return anyMatch;
+                }
+
+                if (urlIsWorkingWebsite()) {
+                    w2gVideos.push(m)
+                } else if (m.startsWith("https://m.facebook")) { // <- Exception for Facebook Mobile links
+                    nonW2GVideos.push(m.replace("m.facebook", "facebook"))
+                } else if (m.startsWith("https://")) { // <- If the URL is not a working website, then it will be a "non W2G" video
+                    nonW2GVideos.push(m)
+                }
+            })
+        })
+
+        await interaction.reply({
+            content: `Starting to build the Watch2Gether room. Total amount of videos: ${w2gVideos.length + nonW2GVideos.length}`
+        })
+
+        console.log(`W2G Videos: ${w2gVideos}\nNon W2G Videos: ${nonW2GVideos}. Posting to Selenium Server...`)
+
+        const w2g = await Server.postToBuildW2G({
+            "urls": w2gVideos.reverse()
+        })
+
+        if (w2g) {
+            await channel.send(`Watch2Gether built. Link: ${w2g.url}`)
+        } else {
+            await channel.send("An error occurred during the building process. Please check to see if the building server is running")
+        }
+    }
+}
---
+++ discord-bot/src/server/Server.ts
@@ -0,0 +1,22 @@
+const axios = require("axios")
+
+export class Server {
+
+    public static async postToBuildW2G(data: any, url: string = "http://localhost:8080/w2g/build"): Promise<{
+        nonWorkingVideos: string[],
+        url: string
+    }> {
+        return await axios.post(url, data, {
+            method: "post",
+            url: "url",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: JSON.stringify(data)
+        }).then(data => {
+            return data.data
+        }).catch(() => {
+            return null
+        })
+    }
+}
